3f5b43f83765e04bea9b827808d9983818c0d555
Darya <127752063+osssinnna@users.noreply.github.com>
2025-12-06 01:35:24 +0300

    pure function

--- src/services/slices/burgerConstructorSlice.ts
+++ src/services/slices/burgerConstructorSlice.ts
@@ -24,24 +24,39 @@ const burgerConstructorSlice = createSlice({
   name: 'burgerConstructor',
   initialState,
   reducers: {
-    addIngredient(state, action: PayloadAction<TIngredient>) {
-      const ingredient = action.payload;
+    addIngredient: {
+      reducer(
+        state,
+        action: PayloadAction<{ ingredient: TIngredient; id: string }>
+      ) {
+        const { ingredient, id } = action.payload;
 
-      if (ingredient.type === 'bun') {
-        state.bun = ingredient;
-      } else {
-        const newItem: ConstructorIngredient = {
-          ...ingredient,
-          id: crypto.randomUUID()
+        if (ingredient.type === 'bun') {
+          state.bun = ingredient;
+        } else {
+          const newItem: ConstructorIngredient = {
+            ...ingredient,
+            id
+          };
+          state.ingredients.push(newItem);
+        }
+      },
+      prepare(ingredient: TIngredient) {
+        return {
+          payload: {
+            ingredient,
+            id: crypto.randomUUID()
+          }
         };
-        state.ingredients.push(newItem);
       }
     },
+
     removeIngredient(state, action: PayloadAction<string>) {
       state.ingredients = state.ingredients.filter(
         (item) => item.id !== action.payload
       );
     },
+
     moveIngredient(state, action: PayloadAction<TMovePayload>) {
       const { fromIndex, toIndex } = action.payload;
       const updated = [...state.ingredients];
@@ -49,6 +64,7 @@ const burgerConstructorSlice = createSlice({
       updated.splice(toIndex, 0, moved);
       state.ingredients = updated;
     },
+
     clearConstructor(state) {
       state.bun = null;
       state.ingredients = [];
