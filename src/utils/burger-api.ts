8dd029f394bd24366e016017717bbef882a01cfd
Darya <127752063+osssinnna@users.noreply.github.com>
2026-02-08 00:45:58 +0300

    deplo

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,8 +1,7 @@
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TOrdersData, TUser } from './types';
 
-const URL =
-  process.env.BURGER_API_URL ?? 'https://norma.education-services.ru/api';
+const URL = 'https://norma.education-services.ru/api';
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
